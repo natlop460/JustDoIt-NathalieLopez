a46ef2142d455b05d84458b2d0c7a25be95028da
Carlos Arias <cariasinter@gmail.com>
2026-01-27 19:26:50 -0600

    Código inicial en Spring Boot

---
+++ src/main/java/teccr/justdoitcloud/JustDoItCloudApplication.java
@@ -0,0 +1,13 @@
+package teccr.justdoitcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustDoItCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustDoItCloudApplication.class, args);
+    }
+
+}
